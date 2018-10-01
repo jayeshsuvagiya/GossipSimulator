3b3b6e75ebc4baa4fee542f50f559d92ec340cf2
jayesh-laptop <work.jayesh@gmail.com>
2018-10-01 16:08:35 -0400

    added 3D Grid topology

--- lib/gossip_simulator/network_simulator.ex
+++ lib/gossip_simulator/network_simulator.ex
@@ -146,19 +146,143 @@ defmodule GossipSimulator.NetworkSimulator do
     s = length(nodes)
     ud = w*w
 
-    GenServer.cast(Enum.at(nodes,0), {:set_neighbours, [Enum.at(nodes, 1), Enum.at(nodes, w)]})
+    GenServer.cast(Enum.at(nodes,0), {:set_neighbours, [Enum.at(nodes, 1), Enum.at(nodes, w),Enum.at(nodes, ud)]})
     IO.puts("0-1,#{w}")
-    GenServer.cast(Enum.at(nodes,w-1), {:set_neighbours, [Enum.at(nodes, w-2), Enum.at(nodes, w-1+w)]})
+    GenServer.cast(Enum.at(nodes,w-1), {:set_neighbours, [Enum.at(nodes, w-2), Enum.at(nodes, w-1+w),Enum.at(nodes, w-1+ud)]})
     IO.puts("#{w-1}-#{w-2},#{(w-1+w)}")
-    GenServer.cast(Enum.at(nodes,ud-w), {:set_neighbours, [Enum.at(nodes, s-w-w), Enum.at(nodes, s-w+1)]})
-    GenServer.cast(Enum.at(nodes,ud-1), {:set_neighbours, [Enum.at(nodes, s-w-w), Enum.at(nodes, s-w+1)]})
+    GenServer.cast(Enum.at(nodes,ud-w), {:set_neighbours, [Enum.at(nodes, ud-w-w), Enum.at(nodes, ud-w+1),Enum.at(nodes, ud-w+ud)]})
+    GenServer.cast(Enum.at(nodes,ud-1), {:set_neighbours, [Enum.at(nodes, ud-2), Enum.at(nodes, ud-w-1),Enum.at(nodes, ud-1+ud)]})
 
-    GenServer.cast(Enum.at(nodes,s-ud), {:set_neighbours, [Enum.at(nodes, s-w-w), Enum.at(nodes, s-w+1)]})
-    GenServer.cast(Enum.at(nodes,s-w), {:set_neighbours, [Enum.at(nodes, s-w-w), Enum.at(nodes, s-w+1)]})
-    GenServer.cast(Enum.at(nodes,s-w), {:set_neighbours, [Enum.at(nodes, s-w-w), Enum.at(nodes, s-w+1)]})
+    GenServer.cast(Enum.at(nodes,s-ud), {:set_neighbours, [Enum.at(nodes, s-ud+1), Enum.at(nodes, s-ud+w),Enum.at(nodes, s-ud-ud)]})
+    GenServer.cast(Enum.at(nodes,s-ud+w-1), {:set_neighbours, [Enum.at(nodes, s-ud+w-2), Enum.at(nodes, s-ud+w-1+w),Enum.at(nodes, s-ud+w-1-ud)]})
+    GenServer.cast(Enum.at(nodes,s-w), {:set_neighbours, [Enum.at(nodes, s-w-w), Enum.at(nodes, s-w+1),Enum.at(nodes, s-w+1-ud)]})
     IO.puts("#{s-w}-#{s-w-w},#{s-w+1}")
-    GenServer.cast(Enum.at(nodes,s-1), {:set_neighbours, [Enum.at(nodes, s-2), Enum.at(nodes, s-w-1)]})
+    GenServer.cast(Enum.at(nodes,s-1), {:set_neighbours, [Enum.at(nodes, s-2), Enum.at(nodes, s-w-1)],Enum.at(nodes, s-1-ud)})
     IO.puts("#{s-1}-#{s-2},#{s-w-1}")
+
+    #EDGES '' '' '' ''
+    Enum.each(1..w-2, fn x ->
+      GenServer.cast(Enum.at(nodes,x), {:set_neighbours, [Enum.at(nodes, x-1),Enum.at(nodes, x+w), Enum.at(nodes, x+1),Enum.at(nodes, x+ud)]})
+      IO.puts("#{x}-#{x-1},#{x+w},#{x+1},#{x+ud}")
+    end)
+
+    Enum.each(s-ud+1..s-ud+w-2, fn x ->
+      GenServer.cast(Enum.at(nodes,x), {:set_neighbours, [Enum.at(nodes, x-1),Enum.at(nodes, x+w), Enum.at(nodes, x+1),Enum.at(nodes, x-ud)]})
+      IO.puts("#{x}-#{x-1},#{x-w},#{x+1},#{x-s+w}")
+    end)
+
+    Enum.each(ud-w+1..ud-2, fn x ->
+      GenServer.cast(Enum.at(nodes,x), {:set_neighbours, [Enum.at(nodes, x-1),Enum.at(nodes, x-w), Enum.at(nodes, x+1),Enum.at(nodes, x+ud)]})
+      IO.puts("#{x}-#{x-1},#{x-w},#{x+1},#{x-s+w}")
+    end)
+
+
+    Enum.each(s-w+1..s-2, fn x ->
+      GenServer.cast(Enum.at(nodes,x), {:set_neighbours, [Enum.at(nodes, x-1),Enum.at(nodes, x-w), Enum.at(nodes, x+1),Enum.at(nodes, x-ud)]})
+      IO.puts("#{x}-#{x-1},#{x-w},#{x+1},#{x-s+w}")
+    end)
+
+    #EDGES | | | |
+    Enum.map_every(w..ud-w-w,w, fn x ->
+      GenServer.cast(Enum.at(nodes,x), {:set_neighbours, [Enum.at(nodes, x-w),Enum.at(nodes, x+1), Enum.at(nodes, x+w),Enum.at(nodes, x+ud)]})
+      IO.puts("#{x}-#{x-w},#{x+1},#{x+w},#{x+w-1}")
+    end)
+
+    Enum.map_every(w-1+w..ud-1-w,w, fn x ->
+      GenServer.cast(Enum.at(nodes,x), {:set_neighbours, [Enum.at(nodes, x-w),Enum.at(nodes, x-1), Enum.at(nodes, x+w),Enum.at(nodes, x+ud)]})
+      IO.puts("#{x}-#{x-w},#{x-1},#{x+w},#{x-w+1}")
+    end)
+
+    Enum.map_every(s-ud+w..s-w-w,w, fn x ->
+      GenServer.cast(Enum.at(nodes,x), {:set_neighbours, [Enum.at(nodes, x-w),Enum.at(nodes, x+1), Enum.at(nodes, x+w),Enum.at(nodes, x-ud)]})
+      IO.puts("#{x}-#{x-w},#{x+1},#{x+w},#{x+w-1}")
+    end)
+
+    Enum.map_every(s-ud+w-1+w..s-1-w,w, fn x ->
+      GenServer.cast(Enum.at(nodes,x), {:set_neighbours, [Enum.at(nodes, x-w),Enum.at(nodes, x-1), Enum.at(nodes, x+w),Enum.at(nodes, x-ud)]})
+      IO.puts("#{x}-#{x-w},#{x-1},#{x+w},#{x-w+1}")
+    end)
+
+
+    #EDGES \ \ \ \
+    Enum.map_every(ud..s-ud-ud,ud, fn x ->
+      GenServer.cast(Enum.at(nodes,x), {:set_neighbours, [Enum.at(nodes, x-ud),Enum.at(nodes, x+1), Enum.at(nodes, x+w),Enum.at(nodes, x+ud)]})
+      IO.puts("#{x}-#{x-w},#{x+1},#{x+w},#{x+w-1}")
+    end)
+
+    Enum.map_every(w-1+ud..s-ud+w-1-ud,ud, fn x ->
+      GenServer.cast(Enum.at(nodes,x), {:set_neighbours, [Enum.at(nodes, x-ud),Enum.at(nodes, x-1), Enum.at(nodes, x+w),Enum.at(nodes, x+ud)]})
+      IO.puts("#{x}-#{x-w},#{x-1},#{x+w},#{x-w+1}")
+    end)
+
+    Enum.map_every(ud-w+ud..s-w-ud,ud, fn x ->
+      GenServer.cast(Enum.at(nodes,x), {:set_neighbours, [Enum.at(nodes, x-ud),Enum.at(nodes, x+1), Enum.at(nodes, x-w),Enum.at(nodes, x+ud)]})
+      IO.puts("#{x}-#{x-w},#{x+1},#{x+w},#{x+w-1}")
+    end)
+
+    Enum.map_every(ud-1+ud..s-1-ud,ud, fn x ->
+      GenServer.cast(Enum.at(nodes,x), {:set_neighbours, [Enum.at(nodes, x-ud),Enum.at(nodes, x-1), Enum.at(nodes, x-w),Enum.at(nodes, x+ud)]})
+      IO.puts("#{x}-#{x-w},#{x-1},#{x+w},#{x-w+1}")
+    end)
+
+    #FACES |_| |_| ......
+    #back
+    Enum.map_every(w+1..ud-w-w+1,w,fn x ->
+      Enum.each(x..x+w-3,fn z->
+        GenServer.cast(Enum.at(nodes,z), {:set_neighbours, [Enum.at(nodes, z-1),Enum.at(nodes, z+1), Enum.at(nodes, z-w),Enum.at(nodes, z+w)]})
+        IO.puts("#{z}-#{z-1},#{z+1},#{z-w},#{z+w}")
+      end)
+    end)
+
+    #front
+    Enum.map_every(s-ud+w+1..s-w-w+1,w,fn x ->
+      Enum.each(x..x+w-3,fn z->
+        GenServer.cast(Enum.at(nodes,z), {:set_neighbours, [Enum.at(nodes, z-1),Enum.at(nodes, z+1), Enum.at(nodes, z-w),Enum.at(nodes, z+w)]})
+        IO.puts("#{z}-#{z-1},#{z+1},#{z-w},#{z+w}")
+      end)
+    end)
+
+    #top
+    Enum.map_every(ud+1..s-ud-ud+1,ud,fn x ->
+      Enum.each(x..x+w-3,fn z->
+        GenServer.cast(Enum.at(nodes,z), {:set_neighbours, [Enum.at(nodes, z-1),Enum.at(nodes, z+1), Enum.at(nodes, z-ud),Enum.at(nodes, z+ud)]})
+        IO.puts("#{z}-#{z-1},#{z+1},#{z-w},#{z+w}")
+      end)
+    end)
+
+    #bottom
+    Enum.map_every(ud-w+ud+1..s-w-ud+1,ud,fn x ->
+      Enum.each(x..x+w-3,fn z->
+        GenServer.cast(Enum.at(nodes,z), {:set_neighbours, [Enum.at(nodes, z-1),Enum.at(nodes, z+1), Enum.at(nodes, z-ud),Enum.at(nodes, z+ud)]})
+        IO.puts("#{z}-#{z-1},#{z+1},#{z-w},#{z+w}")
+      end)
+    end)
+
+    #left
+    Enum.map_every(w+ud..ud-w-w+ud,w,fn x ->
+      Enum.map_every(x..x+(ud*(w-3)),ud,fn z->
+        GenServer.cast(Enum.at(nodes,z), {:set_neighbours, [Enum.at(nodes, z-w),Enum.at(nodes, z+w), Enum.at(nodes, z-ud),Enum.at(nodes, z+ud)]})
+        IO.puts("#{z}-#{z-1},#{z+1},#{z-w},#{z+w}")
+      end)
+    end)
+
+    #Right
+    Enum.map_every(w+ud+w-1..ud-w+ud-1,w,fn x ->
+      Enum.map_every(x..x+(ud*(w-3)),ud,fn z->
+        GenServer.cast(Enum.at(nodes,z), {:set_neighbours, [Enum.at(nodes, z-w),Enum.at(nodes, z+w), Enum.at(nodes, z-ud),Enum.at(nodes, z+ud)]})
+        IO.puts("#{z}-#{z-1},#{z+1},#{z-w},#{z+w}")
+      end)
+    end)
+
+    #Inner
+    Enum.map_every(w+1+ud..s-ud+w+1-ud,ud,fn x ->
+      Enum.map_every(x..x+(ud*(w-3),w,fn y  ->
+        Enum.each(x..x+w-3,fn z->
+          GenServer.cast(Enum.at(nodes,z), {:set_neighbours, [Enum.at(nodes, z-1),Enum.at(nodes, z+1), Enum.at(nodes, z-w),Enum.at(nodes, z+w),Enum.at(nodes,z-ud),Enum.at(nodes,z+ud)]})
+          IO.puts("#{z}-#{z-1},#{z+1},#{z-w},#{z+w}")
+        end)
+      end)
+    end
 
     System.halt(0)
 
